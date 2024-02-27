2e6172eb6f0987578342304f9072d9063e221756
NK1604 <161346893+NK1604@users.noreply.github.com>
2024-02-27 11:10:03 +0530

    Create Small.java

---
+++ Small.java
@@ -0,0 +1,13 @@
+public class Small
+{  
+public static void main(String[] args)   
+{    
+int a=1, b=2, c=3;
+if(a<=b && a<=c)  
+System.out.println("Smalllest no. is"+a);
+else if (b<=a && b<=c)  
+System.out.println("Smalllest no. is"+b);  
+else
+System.out.println("Smalllest no. is"+c);  
+}  
+}
